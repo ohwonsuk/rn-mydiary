492603f1c70d7736df255af05da4f748247510a8
ohwonsuk <wsoh51@gmail.com>
2024-11-17 22:02:34 +0900

    DiaryList-Add버튼추가,Dummy data 표출

--- src/RootApp.js
+++ src/RootApp.js
@@ -1,8 +1,19 @@
 import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
+import React, { useState } from 'react';
 import RootStackNavigation from './navigations/RootStackNavigation';
+import SplashView from './SplashView';
 
 const RootApp = () => {
+  const [initialized, setInitialized] = useState(false);
+
+  if (!initialized) {
+    return (
+      <SplashView
+        onFinishLoad={() => setInitialized(true)}
+      />
+    )
+  }
+
   return (
     <NavigationContainer>
       <RootStackNavigation />
